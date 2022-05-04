ac3c7ab0dec528d535cd6fbdcf6bd236fe9168ab
Matthew Obetz <mobetz@gmail.com>
2022-05-04 10:23:53 -0400

    Add lecture notes, delete commented out broken code.

--- src/VCS.java
+++ src/VCS.java
@@ -47,27 +47,64 @@ public class VCS {
 
         FizzBuzz(100);
 
+
+        /*
+        Instead of keeping our old copies of the code in the file, we can use a "version control system" to track all
+        our changes over time.
+
+
+        This version control software lets us save 'checkpoints' in the code called commits, and at any point we're
+        able to compare different checkpoints or restore a previous point in our history.
+
+        There are many different VCS products out there, but we are going to use one of the most popular, called 'git'.
+
+        We have two ways to interact with git:
+           - We can use the command line interface
+           - We can use a GUI tool, either in our IDE or standalone.
+
+
+        I will try to show examples of how to do things both ways for most of the commands we're going to learn about
+        today.
+
+
+        To create a new repository for our code, the first thing we need to do is initialize a repository in our current
+        folder. If we're in a command prompt, we can do this by typing the words:
+
+        git init
+
+        In IntelliJ, we can go up to the VCS menu on top of our project, select "Enable Version Control Integration...",
+        and select Git from the menu.
+
+        However, by default, the git repo won't save anything. The git repository creates a 'stage' with all the files
+        that exist in this folder. This stage has information about which aren't tracked yet at all, which are tracked
+        and have been changed, and which will be added/removed. We can always view the current status of the stage with
+        the 'git status' command.
+
+        If we want to save a file, we have to do two steps:
+           - Add the file to the list of things from our stage that we want to commit.
+           - Perform the actual commit.
+
+
+        There are a few different ways to add the file. We can either use 'git add <file>' in the terminal, right click
+        the file and select add, or go directly to the commit dialog and check all the checkboxes for the files we want
+        to commit.
+
+        Then, we can commit, either by typing 'git commit -m "<message>"', or by going to the Git menu and selecting the
+        "Commit" option. In the commit dialog, we're able to write a helpful descriptive message of the changes we made.
+         */
+
+         /*
+
+        Now, let's go take a look at our stage again.
+        We can see that our VCS.java file appears in the list of unstaged changes. Let's take a look at what the file
+        looks like when we view it.
+        Our file is showing us line by line what changed in the file:
+           - Green lines are the 'new' version of the code.
+           - Grey/Red lines are the 'old' version of the code.
+
+          */
     }
 
-
-/*
-    public static void FizzBuzz_attempt1(int count_to) {
-        for ( int i=1; i<=count_to; i++) {
-
-            if ( i % 3 == 0 ) {
-                System.out.println("Fizz");
-            } else if ( i % 5 == 0 ) {
-                System.out.println("Buzz");
-            } else if ( i % 3 == 0 && i % 5 == 0 ) {
-                System.out.println("FizzBuzz");
-            } else {
-                System.out.println(i);
-            }
-        }
-    }
-
-    NOTE: THIS DOESN'T WORK PLEASE FIX!!!!!11!
- */
 
     public static void FizzBuzz(int count_to) {
         for ( int i=1; i<=count_to; i++) {
